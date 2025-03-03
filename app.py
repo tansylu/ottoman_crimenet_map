4f3974296fd9222a8c8d42bce623a0c257c76caa
tansylu <tansiluakh@gmail.com>
2025-03-03 19:52:56 +0300

    Connected to firebase, created "markers" collection

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+# filepath: /Users/tansylu/Documents/ottoman_crimenet_map/app.py
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    firebase_config = {
+        "apiKey": os.getenv("FIREBASE_API_KEY"),
+        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+        "appId": os.getenv("FIREBASE_APP_ID")
+    }
+    return render_template('index.html', firebase_config=firebase_config)
 
 if __name__ == '__main__':
     app.run(debug=True)
